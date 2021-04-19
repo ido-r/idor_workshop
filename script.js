605f007224a40c514bb2c5f93e51bea9a7388b98
ido-r <iroet94@gmail.com>
2021-04-19 17:26:34 +0300

    adding filter bar right side

--- script.js
+++ script.js
@@ -12,11 +12,89 @@ function errorLocation(){
     setupMap([34.7818, 32.0853])
 }
 
+var places = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'theatre'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.038659, 38.931567]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'theatre'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.003168, 38.894651]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'bar'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.090372, 38.881189]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'bicycle'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.052477, 38.943951]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'music'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.031706, 38.914581]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'music'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.020945, 38.878241]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'music'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.007481, 38.876516]
+            }
+        }
+    ]
+};
+var filterGroup = document.getElementById('filter-group');
+
 function setupMap(center) {
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: center,
+        center: [-77.04, 38.907],
+        //TODO: center : center,
         zoom: 17
     })
 
@@ -30,9 +108,61 @@ function setupMap(center) {
       
     // map.addControl(directions, 'top-left');     
 
+    map.on('load', function () {
+        // Add a GeoJSON source containing place coordinates and information.
+        map.addSource('places', {
+            'type': 'geojson',
+            'data': places
+        });
+
+        places.features.forEach(function (feature) {
+            var symbol = feature.properties['icon'];
+            var layerID = 'poi-' + symbol;
+
+            // Add a layer for this symbol type if it hasn't been added already.
+            if (!map.getLayer(layerID)) {
+                map.addLayer({
+                    'id': layerID,
+                    'type': 'symbol',
+                    'source': 'places',
+                    'layout': {
+                        // These icons are a part of the Mapbox Light style.
+                        // To view all images available in a Mapbox style, open
+                        // the style in Mapbox Studio and click the "Images" tab.
+                        // To add a new image to the style at runtime see
+                        // https://docs.mapbox.com/mapbox-gl-js/example/add-image/
+                        'icon-image': symbol + '-15',
+                        'icon-allow-overlap': true
+                    },
+                    'filter': ['==', 'icon', symbol]
+                });
+
+                // Add checkbox and label elements for the layer.
+                var input = document.createElement('input');
+                input.type = 'checkbox';
+                input.id = layerID;
+                input.checked = true;
+                filterGroup.appendChild(input);
+
+                var label = document.createElement('label');
+                label.setAttribute('for', layerID);
+                label.textContent = symbol;
+                filterGroup.appendChild(label);
+
+                // When the checkbox changes, update the visibility of the layer.
+                input.addEventListener('change', function (e) {
+                    map.setLayoutProperty(
+                        layerID,
+                        'visibility',
+                        e.target.checked ? 'visible' : 'none'
+                    );
+                });
+            }
+        });
+    });
+
+
     map.on('contextmenu', function(e) {
-        console.log('A click event has occurred at ' + e.lngLat);
-        
         var geojson = {
             type: 'FeatureCollection',
             features: [{
@@ -42,9 +172,9 @@ function setupMap(center) {
                 coordinates: e.lngLat
               },
               properties: {
-                title: 'Mapbox',
                 description: 'DOR IS THE BOSS',
-                icon: 'bar'
+                icon: 'music',
+                title: 'marker'
               }
             }]
           };
@@ -53,21 +183,22 @@ function setupMap(center) {
         geojson.features.forEach(function(marker) {
             var el = document.createElement('div');
             el.className = 'marker';
+            filterGroup.appendChild(el)
     
             // make a marker for each feature and add to the map
             new mapboxgl.Marker(el)
-            .setLngLat(marker.geometry.coordinates)
-            .setPopup(
-                new mapboxgl.Popup({ offset: 25 }) // add popups
-                    .setHTML(
-                    '<h3>' +
-                    marker.properties.title +
-                    '</h3><p>' +
-                    marker.properties.description +
-                    '</p>'
-                    ) 
-            )
-            .addTo(map);
+            .setLngLat(marker.geometry.coordinates).addTo(map);
+            // .setPopup(
+            //     new mapboxgl.Popup({ offset: 25 }) // add popups
+            //         .setHTML(
+            //         '<h3>' +
+            //         marker.properties.title +
+            //         '</h3><p>' +
+            //         marker.properties.description +
+            //         '</p>'
+            //         ) 
+            // )
+            // .addTo(map);
         });
     });
     
